8ba65abcd3f032741e93cea6ff00716242925bda
Sowndiriya <sowndhupapa47@gmail.com>
2025-10-05 21:28:06 +0530

    Create Sort an array.java

---
+++ Sort an array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int []arr={50,20,10,60,40};
+    Arrays.sort(arr);
+  System.out.print("Sorted Array:");
+for(int num:arr){
+  System.out.print(num + " ");
+}
+}
+}
